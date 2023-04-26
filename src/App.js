0a8e85a8b0ecf2ea4324236d4c13393c5ce0c551
YuryHouse <yury.house@yahoo.com>
2023-04-26 20:00:14 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/app.scss';
+import Toolbar from "./components/Toolbar";
+import SettingBar from "./components/SettingBar";
+import Canvas from "./components/Canvas";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className='app'>
+            <Toolbar/>
+            <SettingBar/>
+            <Canvas/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../styles/toolbar.scss';
+import toolState from '../store/toolState';
+import Brush from '../tools/Brush';
+import canvasState from '../store/canvasState';
+import Rect from '../tools/Rect';
+import Circle from '../tools/Circle';
+import Eraser from '../tools/Eraser';
+import Line from '../tools/Line';
+
+const Toolbar = () => {
+    return (
+        <div className={'toolbar'}>
+            <button className={'toolbar__btn brush'} onClick={() => toolState.setTool(new Brush(canvasState.canvas))}/>
+            <button className={'toolbar__btn rect'} onClick={() => toolState.setTool(new Rect(canvasState.canvas))}/>
+            <button className={'toolbar__btn circle'} onClick={() => toolState.setTool(new Circle(canvasState.canvas))}/>
+            <button className={'toolbar__btn eraser'} onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}/>
+            <button className={'toolbar__btn line'} onClick={() => toolState.setTool(new Line(canvasState.canvas))}/>
+            <input style={{marginLeft: 10, cursor: 'pointer'}} type={'color'}/>
+            <button className={'toolbar__btn undo'}/>
+            <button className={'toolbar__btn redo'}/>
+            <button className={'toolbar__btn save'}/>
+        </div>
+    );
+};
+
+export default Toolbar;
